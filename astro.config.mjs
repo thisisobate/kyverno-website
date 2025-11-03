c36e80b61aacb03ee7adee661a533fc51ddb9a26
thisisobate <obasiuche62@gmail.com>
2025-11-03 22:28:13 +0000

    chore: nit fix
    Signed-off-by: thisisobate <obasiuche62@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,19 +6,107 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Kyverno',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
+				// {
+				// 	label: 'Docs',
+				// 	items: [
+				// 		// Each item here is one entry in the navigation menu.
+				// 		{ label: 'Example Guide', slug: 'guides/example' },
+				// 	],
+				// },
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					label: 'Introduction',
+					autogenerate: { directory: 'introduction' },
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Installation',
+					autogenerate: { directory: 'installation' },
+				},
+				{
+					label: 'Applying Policies',
+					collapsed: true,
+					autogenerate: { directory: 'applying-policies' },
+				},
+				{
+					label: 'CRDs',
+					collapsed: true,
+					autogenerate: { directory: 'CRDs' },
+				},
+				{
+					label: 'Exceptions',
+					collapsed: true,
+					autogenerate: { directory: 'exceptions' },
+				},
+				{
+					label: 'CRDs',
+					collapsed: true,
+					autogenerate: { directory: 'CRDs' },
+				},
+				{
+					label: 'High Availability',
+					collapsed: true,
+					autogenerate: { directory: 'high-availability' },
+				},
+				{
+					label: 'Kyverno Chainsaw',
+					collapsed: true,
+					autogenerate: { directory: 'kyverno-chainsaw' },
+				},
+				{
+					label: 'Kyverno CLI',
+					collapsed: true,
+					autogenerate: { directory: 'kyverno-cli' },
+				},
+				{
+					label: 'Kyverno JSON',
+					collapsed: true,
+					autogenerate: { directory: 'kyverno-json' },
+				},
+				{
+					label: 'Kyverno Policy Reporter',
+					collapsed: true,
+					autogenerate: { directory: 'kyverno-policy-reporter' },
+				},
+				{
+					label: 'Monitoring',
+					collapsed: true,
+					autogenerate: { directory: 'monitoring' },
+				},
+				{
+					label: 'Policy Reports',
+					collapsed: true,
+					autogenerate: { directory: 'policy-reports' },
+				},
+				{
+					label: 'policy-types',
+					collapsed: true,
+					autogenerate: { directory: 'policy-types' },
+				},
+				{
+					label: 'Releases',
+					collapsed: true,
+					autogenerate: { directory: 'releases' },
+				},
+				{
+					label: 'Security',
+					collapsed: true,
+					autogenerate: { directory: 'security' },
+				},
+				{
+					label: 'Testing-Policies',
+					collapsed: true,
+					autogenerate: { directory: 'testing-policies' },
+				},
+				{
+					label: 'Tracing',
+					collapsed: true,
+					autogenerate: { directory: 'tracing' },
+				},
+				{
+					label: 'Troubleshooting',
+					autogenerate: { directory: 'troubleshooting' },
 				},
 			],
 		}),
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,11 @@
-import { defineCollection } from 'astro:content';
-import { docsLoader } from '@astrojs/starlight/loaders';
-import { docsSchema } from '@astrojs/starlight/schema';
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { docsLoader } from "@astrojs/starlight/loaders";
+import { docsSchema } from "@astrojs/starlight/schema";
 
 export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+  docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+  policies: defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./src/content/policies" }),
+  }),
 };
