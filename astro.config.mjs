0069a44d2171d5d8614584a56cc17b780b73d356
thisisobate <obasiuche62@gmail.com>
2025-11-04 11:10:32 +0000

    chore: make sidebars to use label names
    Signed-off-by: thisisobate <obasiuche62@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,106 +9,100 @@ export default defineConfig({
 			title: 'Kyverno',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
+
+				// Autogenerate sidebar from the root: '/docs/docs'
 				// {
 				// 	label: 'Docs',
-				// 	items: [
-				// 		// Each item here is one entry in the navigation menu.
-				// 		{ label: 'Example Guide', slug: 'guides/example' },
-				// 	],
+				// 	collapsed: false,
+				// 	autogenerate: { directory: 'docs', collapsed: true, attrs: { style: 'text-transform: capitalize' }},
 				// },
 				{
-					label: 'Docs',
-					collapsed: false,
-					autogenerate: { directory: 'docs', collapsed: true },
+					label: 'Introduction',
+					autogenerate: { directory: 'docs/introduction' },
 				},
-				// {
-				// 	label: 'Installation',
-				// 	autogenerate: { directory: 'installation' },
-				// },
-				// {
-				// 	label: 'Applying Policies',
-				// 	collapsed: true,
-				// 	autogenerate: { directory: 'applying-policies' },
-				// },
-				// {
-				// 	label: 'CRDs',
-				// 	collapsed: true,
-				// 	autogenerate: { directory: 'CRDs' },
-				// },
-				// {
-				// 	label: 'Exceptions',
-				// 	collapsed: true,
-				// 	autogenerate: { directory: 'exceptions' },
-				// },
-				// {
-				// 	label: 'CRDs',
-				// 	collapsed: true,
-				// 	autogenerate: { directory: 'CRDs' },
-				// },
-				// {
-				// 	label: 'High Availability',
-				// 	collapsed: true,
-				// 	autogenerate: { directory: 'high-availability' },
-				// },
-				// {
-				// 	label: 'Kyverno Chainsaw',
-				// 	collapsed: true,
-				// 	autogenerate: { directory: 'kyverno-chainsaw' },
-				// },
-				// {
-				// 	label: 'Kyverno CLI',
-				// 	collapsed: true,
-				// 	autogenerate: { directory: 'kyverno-cli' },
-				// },
-				// {
-				// 	label: 'Kyverno JSON',
-				// 	collapsed: true,
-				// 	autogenerate: { directory: 'kyverno-json' },
-				// },
-				// {
-				// 	label: 'Kyverno Policy Reporter',
-				// 	collapsed: true,
-				// 	autogenerate: { directory: 'kyverno-policy-reporter' },
-				// },
-				// {
-				// 	label: 'Monitoring',
-				// 	collapsed: true,
-				// 	autogenerate: { directory: 'monitoring' },
-				// },
-				// {
-				// 	label: 'Policy Reports',
-				// 	collapsed: true,
-				// 	autogenerate: { directory: 'policy-reports' },
-				// },
-				// {
-				// 	label: 'policy-types',
-				// 	collapsed: true,
-				// 	autogenerate: { directory: 'policy-types' },
-				// },
-				// {
-				// 	label: 'Releases',
-				// 	collapsed: true,
-				// 	autogenerate: { directory: 'releases' },
-				// },
-				// {
-				// 	label: 'Security',
-				// 	collapsed: true,
-				// 	autogenerate: { directory: 'security' },
-				// },
-				// {
-				// 	label: 'Testing-Policies',
-				// 	collapsed: true,
-				// 	autogenerate: { directory: 'testing-policies' },
-				// },
-				// {
-				// 	label: 'Tracing',
-				// 	collapsed: true,
-				// 	autogenerate: { directory: 'tracing' },
-				// },
-				// {
-				// 	label: 'Troubleshooting',
-				// 	autogenerate: { directory: 'troubleshooting' },
-				// },
+				{
+					label: 'Installation',
+					autogenerate: { directory: 'docs/installation' },
+				},
+				{
+					label: 'Applying Policies',
+					collapsed: true,
+					autogenerate: { directory: 'docs/applying-policies' },
+				},
+				{
+					label: 'CRDs',
+					collapsed: true,
+					autogenerate: { directory: 'docs/CRDs' },
+				},
+				{
+					label: 'Exceptions',
+					collapsed: true,
+					autogenerate: { directory: 'docs/exceptions' },
+				},
+				{
+					label: 'High Availability',
+					collapsed: true,
+					autogenerate: { directory: 'docs/high-availability' },
+				},
+				{
+					label: 'Kyverno Chainsaw',
+					collapsed: true,
+					autogenerate: { directory: 'docs/kyverno-chainsaw' },
+				},
+				{
+					label: 'Kyverno CLI',
+					collapsed: true,
+					autogenerate: { directory: 'docs/kyverno-cli' },
+				},
+				{
+					label: 'Kyverno JSON',
+					collapsed: true,
+					autogenerate: { directory: 'docs/kyverno-json' },
+				},
+				{
+					label: 'Kyverno Policy Reporter',
+					collapsed: true,
+					autogenerate: { directory: 'docs/kyverno-policy-reporter' },
+				},
+				{
+					label: 'Monitoring',
+					collapsed: true,
+					autogenerate: { directory: 'docs/monitoring' },
+				},
+				{
+					label: 'Policy Reports',
+					collapsed: true,
+					autogenerate: { directory: 'docs/policy-reports' },
+				},
+				{
+					label: 'Policy-Types',
+					collapsed: true,
+					autogenerate: { directory: 'docs/policy-types' },
+				},
+				{
+					label: 'Releases',
+					collapsed: true,
+					autogenerate: { directory: 'docs/releases' },
+				},
+				{
+					label: 'Security',
+					collapsed: true,
+					autogenerate: { directory: 'docs/security' },
+				},
+				{
+					label: 'Testing-Policies',
+					collapsed: true,
+					autogenerate: { directory: 'docs/testing-policies' },
+				},
+				{
+					label: 'Tracing',
+					collapsed: true,
+					autogenerate: { directory: 'docs/tracing' },
+				},
+				{
+					label: 'Troubleshooting',
+					autogenerate: { directory: 'docs/troubleshooting' },
+				},
 			],
 		}),
 	],
